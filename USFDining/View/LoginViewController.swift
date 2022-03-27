9180fe826c57134eac1fa08c552e8c4c0cdb94f7
MattSharp <m5sharp@icloud.com>
2022-03-27 14:29:55 -0400

    final edit

--- USFDining/View/LoginViewController.swift
+++ USFDining/View/LoginViewController.swift
@@ -49,6 +49,10 @@ class LoginViewController: UIViewController {
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     /*
     // MARK: - Navigation
 
